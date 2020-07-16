238749fd552edafbd572ccfa6d1ba6b8b67053e5
bei-zhi <s_jiajun1999@163.com>
2020-07-16 19:49:05 +0800

    controller类

--- backend/src/main/java/com/weizhiblog/service/UserService.java
+++ backend/src/main/java/com/weizhiblog/service/UserService.java
@@ -15,9 +15,25 @@ public class UserService {
     public Integer signUpUser(User user){
         return 1;
     }
-
+    public Integer loginUser(User user){
+        return 1;
+    }
     public List<User> getAllUsers(){
         System.out.println(userMapper.listUsers());
         return null;
     }
+    public Integer del_allUser(User user){
+        return 1;
+    }
+    public Integer deleteUser(User user) {return 1; }
+
+    public Integer changeUser(User user) {return 1; }
+
+    public Integer get_allUser(User user) {return 1; }
+
+
+
+    public Integer change_statusUser(User user) {return 1; }
+
+    public Integer change_pwdUser(User user) {return 1; }
 }
