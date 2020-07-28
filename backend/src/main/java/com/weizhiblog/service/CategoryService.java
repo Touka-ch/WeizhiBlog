0e5af6a7dfeadfc56a816e8a658c8db48cbc59a5
Touka_ <36352255+Touka-ch@users.noreply.github.com>
2020-07-28 18:11:16 +0800

    DataService,ArticleService

--- backend/src/main/java/com/weizhiblog/service/CategoryService.java
+++ backend/src/main/java/com/weizhiblog/service/CategoryService.java
@@ -16,6 +16,7 @@ public class CategoryService {
 
     /**
      * 新建目录
+     *
      * @param category 目录信息
      * @return 是否新建成功
      * 关于 status
@@ -25,15 +26,15 @@ public class CategoryService {
      * -3：目录名称已存在
      */
     public ResponseBean insertCategory(Category category) {
-        try{
-            if(categoryMapper.selectByPrimaryKey(category.getId())!=null)
+        try {
+            if (categoryMapper.selectByPrimaryKey(category.getId()) != null)
                 return ResponseBean.builder().status(-2).message("目录id已存在").build();
-            if(categoryMapper.getCategoryByCateName(category.getCateName())!=null)
+            if (categoryMapper.getCategoryByCateName(category.getCateName()) != null)
                 return ResponseBean.builder().status(-3).message("目录名称已存在").build();
-            return categoryMapper.insert(category)==1?
-                    ResponseBean.builder().status(1).message("创建成功").build():
+            return categoryMapper.insert(category) == 1 ?
+                    ResponseBean.builder().status(1).message("创建成功").build() :
                     ResponseBean.builder().status(0).message("未知原因！").build();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseBean.builder().status(-1).message("服务器错误！").build();
         }
@@ -41,6 +42,7 @@ public class CategoryService {
 
     /**
      * 删除指定目录
+     *
      * @param id 目录id
      * @return 是否删除成功
      * 关于 status
@@ -49,13 +51,13 @@ public class CategoryService {
      * -2：删除目录不存在
      */
     public ResponseBean deleteCategory(int id) {
-        try{
-            if(categoryMapper.selectByPrimaryKey(id)==null)
+        try {
+            if (categoryMapper.selectByPrimaryKey(id) == null)
                 return ResponseBean.builder().status(-2).message("目录不已存在").build();
-            return categoryMapper.deleteByPrimaryKey(id)==1?
-                    ResponseBean.builder().status(1).message("删除成功").build():
+            return categoryMapper.deleteByPrimaryKey(id) == 1 ?
+                    ResponseBean.builder().status(1).message("删除成功").build() :
                     ResponseBean.builder().status(0).message("未知原因！").build();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseBean.builder().status(-1).message("服务器错误！").build();
         }
@@ -63,6 +65,7 @@ public class CategoryService {
 
     /**
      * 修改目录名称
+     *
      * @param category 目录信息
      * @return 是否修改成功
      * 关于 status
@@ -72,15 +75,15 @@ public class CategoryService {
      * -3：目录名称已使用
      */
     public ResponseBean updateCategory(Category category) {
-        try{
-            if(categoryMapper.selectByPrimaryKey(category.getId())!=null)
+        try {
+            if (categoryMapper.selectByPrimaryKey(category.getId()) != null)
                 return ResponseBean.builder().status(-2).message("目录id已使用").build();
-            if(categoryMapper.getCategoryByCateName(category.getCateName())!=null)
+            if (categoryMapper.getCategoryByCateName(category.getCateName()) != null)
                 return ResponseBean.builder().status(-3).message("目录名称已存在").build();
-            return categoryMapper.updateByPrimaryKey(category)==1?
-                    ResponseBean.builder().status(1).message("更新成功").build():
+            return categoryMapper.updateByPrimaryKey(category) == 1 ?
+                    ResponseBean.builder().status(1).message("更新成功").build() :
                     ResponseBean.builder().status(0).message("未知原因！").build();
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseBean.builder().status(-1).message("服务器错误！").build();
         }
@@ -88,11 +91,12 @@ public class CategoryService {
 
     /**
      * 找出用户在指定目录下所有文章
+     *
      * @param uid 用户id
      * @param cid 目录id
      * @return 是否查询成功
      */
-    public ResponseBean getArticleByCategory(int uid,int cid) {
+    public ResponseBean getArticleByCategory(int uid, int cid) {
         return ResponseBean.builder().status(1).message("").build();
     }
 
